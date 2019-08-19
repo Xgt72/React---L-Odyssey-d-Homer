5436a29b3ffccad9b7d06e01432a9d48a4e8e70b
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-08-19 14:22:32 +0200

    remove of the email in App.js

--- front/homer-odyssey/src/App.js
+++ front/homer-odyssey/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Homer's Odyssey</h1>
       </header>
-      <SignUp email="xa@gmail.com" />
+      <SignUp />
     </div>
   );
 }
