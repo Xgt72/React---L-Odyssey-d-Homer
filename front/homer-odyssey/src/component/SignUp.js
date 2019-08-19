4532a19b31d10900c8a2287208c185747225399f
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-08-19 15:10:08 +0200

    solution for the quest 020

--- front/homer-odyssey/src/component/SignUp.js
+++ front/homer-odyssey/src/component/SignUp.js
@@ -4,28 +4,106 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: ""
+            email: "",
+            password: "",
+            passwordBis: "",
+            firstname: "",
+            lastname: ""
         };
-        this.updateEmailField = this.updateEmailField.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.updatePasswordBis = this.updatePasswordBis.bind(this);
+        this.updateFirstname = this.updateFirstname.bind(this);
+        this.updateLastname = this.updateLastname.bind(this);
+        this.submitForm = this.submitForm.bind(this);
     }
-    updateEmailField = (event) => {
+
+    updateEmail = (event) => {
         this.setState({
             [event.target.name]: event.target.value,
         });
     };
 
+    updatePassword = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    updatePasswordBis = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    updateFirstname = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    updateLastname = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    submitForm = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    };
+
     render() {
         return (
-            <div>
-                <h1>{this.state.email}</h1>
+            <form onSubmit={this.submitForm}>
+                <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+                <label htmlFor="email">Email</label>
                 <input
                     id="email"
                     type="email"
                     name="email"
-                    onChange={this.updateEmailField}
+                    onChange={this.updateEmail}
                     value={this.state.email}
+                    required
                 />
-            </div>
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={this.updatePassword}
+                    value={this.state.password}
+                    required
+                />
+                <label htmlFor="passwordBis">Confirm Password</label>
+                <input
+                    id="passwordBis"
+                    type="password"
+                    name="passwordBis"
+                    onChange={this.updatePasswordBis}
+                    value={this.state.passwordBis}
+                    required
+                />
+                <label htmlFor="firstname">Firstname</label>
+                <input
+                    id="firstname"
+                    type="firstname"
+                    name="firstname"
+                    onChange={this.updateFirstname}
+                    value={this.state.firstname}
+                    required
+                />
+                <label htmlFor="lastname">Lastname</label>
+                <input
+                    id="lastname"
+                    type="lastname"
+                    name="lastname"
+                    onChange={this.updateLastname}
+                    value={this.state.lastname}
+                    required
+                />
+                <input className="submitButton" type="submit" value="submit" />
+            </form>
         )
     };
 }
