c136961d3c51dfe7c47a8f8682f3324610beeac2
Xgt72 <xavier.georget@wildcodeschool.fr>
2019-08-21 09:44:29 +0200

    solution for quest 080

---
+++ back/helpers/passport.js
@@ -0,0 +1,49 @@
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const connection = require("./db.js");
+const bcrypt = require("bcrypt");
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password',
+        session: false
+    },
+    function (email, password, cb) {
+
+        connection.query("SELECT password, firstname, lastname FROM users WHERE email = ?", email, (err, results) => {
+            if (err) {
+                return cb(err);
+            } else {
+                console.log("1", results);
+                if (results[0] == undefined) {
+                    return cb(null, false, { message: 'Incorrect email or password.' });
+                } else {
+                    console.log("2");
+                    if (bcrypt.compareSync("homer" + password, results[0].password)) {
+                        const user = {
+                            email: email
+                        };
+                        return cb(null, user);
+                    } else {
+                        return cb(null, false, { message: 'Incorrect email or password.' });
+                    }
+                }
+            }
+        });
+    }
+));
+
+passport.use(new JWTStrategy(
+    {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: "homerS"
+    },
+    function(jwtPayload, cb) {
+        return cb(null, jwtPayload);
+    }
+));
+
+module.exports = passport;
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 const connection = require("../../helpers/db.js");
+const bcrypt = require("bcrypt");
+const passport = require("../../helpers/passport.js");
+const jwt = require("jsonwebtoken");
 
-router.post("/signup", (req, res, next) => {
+router.post("/signup", (req, res) => {
     const user = req.body;
+    user.password = bcrypt.hashSync("homer" + user.password , 10);
     connection.query("INSERT INTO users SET ?", user, (err, results) => {
         if (err) {
             res.status(500).json({flash: err.message});
@@ -12,5 +16,20 @@ router.post("/signup", (req, res, next) => {
         }
     });
 });
+
+router.post("/signin", (req, res) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        if (!user) {
+            return res.status(400).json({message: info.message});
+        }
+        const token = jwt.sign(user, 'homerS', { expiresIn: 60 * 5 });
+        return res.json({user, token});
+    })(req, res);
+});
+
+
 
 module.exports = router;
